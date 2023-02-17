8b9b651d89a58cacbe3ebc07b848d7599a511027
Moyu0730 <aslanchen0730@gmail.com>
2023-02-17 15:26:56 +0800

    v1.2 Adjust Something in LimelightSubsystem

--- src/main/java/frc/robot/subsystems/LimelightSubsystem.java
+++ src/main/java/frc/robot/subsystems/LimelightSubsystem.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems;
 
+import org.apache.commons.lang3.tuple.ImmutableTriple;
+
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
@@ -8,83 +10,99 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LimelightSubsystem extends SubsystemBase{
     
-    NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
-    NetworkTable table_2 = NetworkTableInstance.create().getTable("limelight");
+    NetworkTable mTable = NetworkTableInstance.getDefault().getTable("limelight");
+    NetworkTable mTable_2 = NetworkTableInstance.create().getTable("limelight");
     
-    NetworkTableEntry tx = table.getEntry("tx");
-    NetworkTableEntry ty = table.getEntry("ty");
-    NetworkTableEntry tv = table.getEntry("tv");
-    NetworkTableEntry ta = table.getEntry("ta");
+    NetworkTableEntry tx_1 = mTable.getEntry("tx");
+    NetworkTableEntry ty_1 = mTable.getEntry("ty");
+    NetworkTableEntry tv_1 = mTable.getEntry("tv");
+    NetworkTableEntry ta_1 = mTable.getEntry("ta");
 
-    NetworkTableEntry tx_2 = table_2.getEntry("tx");
-    NetworkTableEntry ty_2 = table_2.getEntry("ty");
-    NetworkTableEntry tv_2 = table_2.getEntry("tv");
-    NetworkTableEntry ta_2 = table_2.getEntry("ta");
+    NetworkTableEntry tx_2 = mTable_2.getEntry("tx");
+    NetworkTableEntry ty_2 = mTable_2.getEntry("ty");
+    NetworkTableEntry tv_2 = mTable_2.getEntry("tv");
+    NetworkTableEntry ta_2 = mTable_2.getEntry("ta");
     
-    double x, x_2;
-    double y, y_2;
-    double a, a_2;
-    boolean v, v_2;
+    double mX_1, mY_1, mA_1, mX_2, mY_2, mA_2;
+    boolean mV_1, mV_2;
 
     @Override
     public void periodic(){
 
-        x = tx.getDouble(0);
-        y = ty.getDouble(0);
-        a = ta.getDouble(0);
-        v = tv.getBoolean(false);
+        mX_1 = tx_1.getDouble(0);
+        mY_1 = ty_1.getDouble(0);
+        mA_1 = ta_1.getDouble(0);
+        mV_1 = tv_1.getBoolean(false);
 
-        x_2 = tx_2.getDouble(0);
-        y_2 = ty_2.getDouble(0);
-        a_2 = ta_2.getDouble(0);
-        v_2 = tv_2.getBoolean(false);
+        mX_2 = tx_2.getDouble(0);
+        mY_2 = ty_2.getDouble(0);
+        mA_2 = ta_2.getDouble(0);
+        mV_2 = tv_2.getBoolean(false);
     
-        SmartDashboard.putNumber("tx", x);
-        SmartDashboard.putNumber("ty", y);
-        SmartDashboard.putNumber("ta", a);
-        SmartDashboard.putBoolean("tv", v);
+        SmartDashboard.putNumber("tx_1", mX_1);
+        SmartDashboard.putNumber("ty_1", mY_1);
+        SmartDashboard.putNumber("ta_1", mA_1);
+        SmartDashboard.putBoolean("tv_1", mV_1);
 
-        SmartDashboard.putNumber("tx_2", x_2);
-        SmartDashboard.putNumber("ty_2", y_2);
-        SmartDashboard.putNumber("ta_2", a_2);
-        SmartDashboard.putBoolean("tv_2", v_2);
-
+        SmartDashboard.putNumber("tx_2", mX_2);
+        SmartDashboard.putNumber("ty_2", mY_2);
+        SmartDashboard.putNumber("ta_2", mA_2);
+        SmartDashboard.putBoolean("tv_2", mV_2);
     }
 
     /**
      * Cone_1
      */
     public double getX_1(){
-        return x;
+        return mX_1;
     }
+    
     /**
      * Cone_1
      */
     public double getY_1(){
-        return y;
+        return mY_1;
     }
+
+    /**
+     * Cone_1
+     */
+    public double getA_1(){
+        return mA_1;
+    }
+
     /**
      * Cone_1
      */
     public boolean getV_1(){
-        return v;
+        return mV_1;
     }
+    
     /**
      * Cone_2
      */
     public double getX_2(){
-        return x_2;
+        return mX_2;
     }
-  /**
+
+    /**
      * Cone_2
      */
     public double getY_2(){
-        return y_2;
+        return mY_2;
     }
-  /**
+
+    /**
+     * Cone_2
+     */
+    public double getA_2(){
+        return mA_2;
+    }
+
+    /**
      * Cone_2
      */
     public boolean getV_2(){
-        return v_2;
+        return mV_2;
     }
 }
